1ff9a0c786906ba0df65c4fa172c93a602bf5429
migdalius <nyjepl@gmail.com>
2023-03-25 14:14:42 +0100

    category page (static)

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import GigCard from "../../components/gigCard/GigCard";
 import "./Gigs.scss";
+import { gigs } from "../../data";
 
 const Gigs = () => {
-  return <div>Gigs</div>;
+  const [sort, setSort] = useState("sales");
+  const [open, setOpen] = useState(false);
+
+  const reSort = (type) => {
+    setSort(type);
+    setOpen(false);
+  };
+
+  return (
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">
+          Kategoria {">"} Inne {">"} Produkt
+        </span>
+        <h1>AI Artist</h1>
+        <p>Lorem of Lorem and Lorem in Lorem and Lorem</p>
+        <div className="menu">
+          <div className="left">
+            <span>Budged</span>
+            <input type="text" placeholder="min" />
+            <input type="text" placeholder="max" />
+            <button>Applay</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">SortBy</span>
+            <span className="sortType">
+              {sort === "sales" ? "Best Selling" : "Newest"}
+            </span>
+            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="cards">
+          {gigs.map((gig) => (
+            <GigCard key={gig.id} item={gig} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Gigs;
